ac9ac9f664eeb0c75dfddf4e821ecb8fa5977748
Ansla96 <ansla96@gmail.com>
2022-05-08 20:52:29 +0200

    Update KubAgent.pp

--- KubAgent.pp
+++ KubAgent.pp
@@ -2,4 +2,16 @@ node 'default' {
   include 'docker'
   include 'apt'
   include 'stdlib'
+  
+  user { 'Kirk':
+  ensure => present,
+  home => '/home/Kirk',
+  password => 'Kirk123',
+  }
+  
+  user { 'Rich':
+  ensure => present,
+  home => '/home/Rich',
+  password => 'Rich123',
+  } 
 }
